fc1b116f92121ded3a034c6b1f3015e9e71bd9f6
Brahmia-Malek <131770411+Brahmia-Malek@users.noreply.github.com>
2023-06-21 23:51:38 +0200

    Add files via upload

---
+++ week1/day3/core/first-project/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <fieldset>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need to do :</h2>
+    <ul>
+      <li>learn React</li>
+      <li>Climb Mt.Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </fieldset>
+    </div>
+  );
+}
+
+export default App;
+
