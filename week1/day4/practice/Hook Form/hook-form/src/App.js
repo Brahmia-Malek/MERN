df2576f4cee57684de1dc44713416d7c64b65373
Brahmia-Malek <131770411+Brahmia-Malek@users.noreply.github.com>
2023-07-02 19:50:24 +0200

    Add files via upload

---
+++ week1/day4/practice/Hook Form/hook-form/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import HookForm from './Components/HookForm';
+
+function App() {
+  return (
+    <div className="App">
+      <HookForm/>
+
+    </div>
+  );
+}
+
+export default App;
